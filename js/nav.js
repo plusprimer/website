422996c54b672aa2f771b9457bcdd91f1b423e97
plusprimer <766081399@qq.com>
2015-05-09 16:03:01 +0800

    v0.0.6
    文件布局统一更改并增加上一版本的子菜单功能

---
+++ js/nav.js
@@ -0,0 +1,14 @@
+$(document).ready(function() {
+	$("#ul1").click(function() {
+		$(".tog1").toggle("showOrHide");
+	});
+	$("#ul2").click(function() {
+		$(".tog2").toggle("400");
+	});
+	$("#ul3").click(function() {
+		$(".tog3").toggle("400");
+	});
+	$("#ul4").click(function() {
+		$(".tog4").toggle("400");
+	});
+});
